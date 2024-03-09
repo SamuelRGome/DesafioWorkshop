fb5bc36d7ab07b1dfd4f55a25109b5a730b6af29
Samuel Rufino <116453643+SamuelRGome@users.noreply.github.com>
2024-03-09 18:59:01 -0300

    Bux Fixes

--- Pokedex/Pokelib/views.py
+++ Pokedex/Pokelib/views.py
@@ -20,8 +20,7 @@ def index(request):
             "peso": str(list_of_data['weight']),
             "sprite": str(list_of_data['sprites']['front_default']),
         }
-
         print(data)
     else:
         data = {}
-    return render(request, 'main/index.html', data)
+    return render(request, 'main/index.html', 'Pokelib\models.py', data)
